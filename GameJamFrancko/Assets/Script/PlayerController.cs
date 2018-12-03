e33fb998fa6fb96c86a85ce1d52ed7ab107464d4
Unknown <matthieu.manuellan@gmail.com>
2018-12-03 15:30:31 +0100

    SetupPlayerController

---
+++ GameJamFrancko/Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D _rb;
+    private BoxCollider2D _boxCollider;
+
+    private int _life;
+    [SerializeField]
+    private float _speed;
+
+	void Start () {
+        _rb = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	
+	void Update () {
+
+        Move();
+
+	}
+
+    private void Move()
+    {
+        float _moveX = Input.GetAxis("Horizontal");
+        float _moveY = Input.GetAxis("Vertical");
+        transform.Translate(new Vector2(_moveX * _speed, _moveY * _speed));
+    }
+}
