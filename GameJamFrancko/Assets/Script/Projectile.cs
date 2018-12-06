236c653b95670f511fe7703378cf7ae2c18bc50d
Matt <matthieu.manuellan@gmail.com>
2018-12-07 00:00:59 +0100

    On abandonne le scale

--- GameJamFrancko/Assets/Script/Projectile.cs
+++ GameJamFrancko/Assets/Script/Projectile.cs
@@ -9,33 +9,19 @@ public class Projectile : MonoBehaviour {
     private float _speed = 4;
     private bool _canMove = false;
 
-    private float _scale;
-    private float _growingScale;
-    private float _topScale;
-
-    private Vector3 _center;
-
-    private bool _up = false;
-    private bool _down = false;
 
     private CircleCollider2D _collider;
 
     private void Start()
     {
         _collider = GetComponent<CircleCollider2D>();
-        _scale = transform.localScale.x;
-        _growingScale = _scale;
-        _topScale = _scale * 2;
-
     }
 
     public void FlyTo(Vector3 target)
     {
         _target = target;
         _target.z = 0;
-        _center = Vector3.Lerp(transform.position,_target,0.5f);
         _canMove = true;
-        _up = true;
     }
 
     private void Update()
