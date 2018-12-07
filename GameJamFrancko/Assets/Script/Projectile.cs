e663958b746c3361340f1180ca41d5f834f3d57b
Unknown <matthieu.manuellan@gmail.com>
2018-12-07 18:26:02 +0100

    fix Projectile

--- GameJamFrancko/Assets/Script/Projectile.cs
+++ GameJamFrancko/Assets/Script/Projectile.cs
@@ -8,6 +8,7 @@ public class Projectile : MonoBehaviour {
     private Vector3 _target;
     private float _speed = 4;
     private bool _canMove = false;
+    private bool _canDammage = false;
 
     [SerializeField]
     private int _damage;
@@ -25,6 +26,7 @@ public class Projectile : MonoBehaviour {
         _target = target;
         _target.z = 0;
         _canMove = true;
+        _collider.enabled = false;
     }
 
     private void Update()
@@ -34,18 +36,29 @@ public class Projectile : MonoBehaviour {
         { 
             transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
         }
-        if (transform.position == _target)
+        if (transform.position.x >= _target.x -1.0f)
         {
-            _canMove = false;
+            _canDammage = true;
             _collider.enabled = true;
         }
+        if(transform.position == _target)
+        {
+            _canMove = false;
+            StartCoroutine("Wait");
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(1.0f);
+        _canDammage = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Ennemi")
+        if (collision.tag == "Ennemi" && _canDammage)
         {
-            collision.gameObject.GetComponent<Sanglier>().DealDammage(_damage);
+            collision.GetComponent<Sanglier>().DealDammage(1);
             Destroy(gameObject);
         }
     }
--- GameJamFrancko/Assets/Script/Sanglier.cs
+++ GameJamFrancko/Assets/Script/Sanglier.cs
@@ -50,4 +50,5 @@ public class Sanglier : MonoBehaviour {
     }
 
     
+
 }
