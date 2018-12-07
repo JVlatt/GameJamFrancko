87366f7b81e63d20d65c463e2156a8c1411e5e6b
Unknown <matthieu.manuellan@gmail.com>
2018-12-07 16:40:37 +0100

    manette presque fini

--- GameJamFrancko/Assets/Script/Catapulte.cs
+++ GameJamFrancko/Assets/Script/Catapulte.cs
@@ -6,6 +6,11 @@ public class Catapulte : MonoBehaviour {
 
     private GameObject _ammoObject;
     public bool _isLoaded { get; private set;}
+    public GameObject _target;
+
+    [SerializeField]
+    private float _speed;
+
 
     public void Reload(GameObject ammo)
     {
@@ -20,9 +25,22 @@ public class Catapulte : MonoBehaviour {
 
     public void Shoot()
     {
-
-        _ammoObject.GetComponent<Projectile>().FlyTo(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        _ammoObject.GetComponent<Projectile>().FlyTo(_target.transform.position);
         _ammoObject = null;
         _isLoaded = false;
+    }
+
+    private void Update()
+    {
+        MoveTarget();
+    }
+
+    private void MoveTarget()
+    {
+        Vector3 _move = new Vector3(Input.GetAxis("RStickX"), Input.GetAxis("RStickY"));
+        if (_move.magnitude >= 0.2f)
+        {
+            _target.transform.Translate(_move * _speed * Time.deltaTime, Space.World);
+        }
     }
 }
