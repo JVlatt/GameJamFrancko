9da4aa43216d437f42e1aa87f73ed950d8f22c5f
Unknown <cedric.chauvin99@gmail.com>
2018-12-07 18:49:04 +0100

    tuto

--- GameJamFrancko/Assets/Script/TutoControler.cs
+++ GameJamFrancko/Assets/Script/TutoControler.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutoControler : MonoBehaviour {
 
     public static TutoControler _myTuto;
 
     public int _tutoState { get; private set; }
+
+    [SerializeField]
+    private Text text;
 
     private Animator _myAnimator;
 
@@ -24,6 +28,24 @@ public class TutoControler : MonoBehaviour {
         {
             state++;
             _myAnimator.SetInteger("CurrentState", state);
+            switch (state)
+            {
+                case 2:
+                    text.text = "Éliminez-le";
+                    break;
+                case 3:
+                    text.text = "La catapulte est vide ! Vous devez recharger !";
+                    break;
+                case 4:
+                    text.text = "Servez vous des cadavres comme munition";
+                    break;
+                case 5:
+                    text.text = "Cet ennemi est plus résistant ";
+                    break;
+                case 6:
+                    text.text = "Les sangliers chargent !";
+                    break;
+            }
         }
     }
 
